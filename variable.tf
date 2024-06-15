a9ef459f7e3ac48219637b496d3d9d8946b7a62e
Yuvaraj9607 <yuvi9607@gmail.com>
2024-06-15 12:18:33 +0530

    update

--- variable.tf
+++ variable.tf
@@ -10,23 +10,23 @@ variable "secret_key" {
 }
 variable "cidr_vpc" {
   
-  default = "10.0.0.0/16"
+  default = "10.0.0.0/24"
 }
 variable "subpub1" {
   
-  default = "10.0.1.0/16"
+  default = "10.0.1.0/24"
 }
 variable "subpub2" {
   
-  default = "10.0.2.0/16"
+  default = "10.0.2.0/24"
 }
 variable "subpvt1" {
 
-  default = "10.0.3.0/16"
+  default = "10.0.3.0/24"
 }
 variable "subpvt2" {
  
-  default = "10.0.4.0/16"
+  default = "10.0.4.0/24"
 }
 variable "az_subnet1" {
   default = "ap-south-1a"
